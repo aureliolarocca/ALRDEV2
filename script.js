8973d68304f69e0e997c6e73612be3da80b77bad
Aurelio <laroccaaurelio1@gmail.com>
2025-07-11 08:21:07 +0200

    Implementazioni js

--- script.js
+++ script.js
@@ -1,11 +1,66 @@
-const hamMenu = document.querySelectorAll(".ham");
-hamMenu.forEach((x) => {
-  x.addEventListener("click", () => {
-    const menu = document.querySelector(".menu");
-    if (menu.classList.contains("active-ham-men")) {
-      menu.classList.remove("active-ham-men");
-    } else {
-      menu.classList.add("active-ham-men");
-    }
-  });
+// Dichiarazioni e funzione per Hamburger menu
+
+const hamMenu = document.querySelector(".ham-men");
+const closeBtnHam = document.querySelector(".close-btn-ham");
+const menu = document.querySelector(".menu");
+const ham = document.querySelectorAll(".ham");
+
+hamMenu.addEventListener("click", () => {
+  if (menu.classList.contains("active-ham-men")) {
+    menu.classList.remove("active-ham-men");
+    ham.forEach((span) => {
+      span.style.display = "flex";
+    });
+    closeBtnHam.style.display = "none";
+    document.querySelector("body").style.overflow = "auto";
+  } else {
+    ham.forEach((span) => {
+      span.style.display = "none";
+    });
+    closeBtnHam.style.display = "flex";
+    menu.classList.add("active-ham-men");
+    document.querySelector("body").style.overflow = "hidden";
+  }
+});
+
+// Dichiarazioni per bottone cambio colore pagina
+const containerBtnLight = document.querySelector(".container-btn-light");
+const btnLight = document.querySelector(".btn-light");
+const light = document.querySelector(".light");
+const imgLight = document.getElementById("btnImage");
+
+containerBtnLight.addEventListener("click", () => {
+  // Dichiarazione di elementi che cambiano colore
+  const body = document.querySelector("body");
+  const allP = document.querySelectorAll("p");
+
+  const toDarkToWhiteBackground = [body];
+  const toWhiteToDarkText = [...allP];
+
+  if (light.classList.contains("light-active")) {
+    light.classList.remove("light-active");
+    imgLight.src = "moon-icon-0.png";
+
+    toDarkToWhiteBackground.forEach((x) => {
+      x.classList.remove("changeBackground");
+    });
+
+    toWhiteToDarkText.forEach((x) => {
+      x.classList.remove("changeColorText");
+    });
+    menu.classList.remove("changeShadowActiveMenu");
+  } else {
+    light.classList.add("light-active");
+    imgLight.src =
+      "pngtree-simple-sunshine-clipart-image-png-image_6527088.png";
+
+    toDarkToWhiteBackground.forEach((x) => {
+      x.classList.add("changeBackground");
+    });
+
+    toWhiteToDarkText.forEach((x) => {
+      x.classList.add("changeColorText");
+    });
+    menu.classList.add("changeShadowActiveMenu");
+  }
 });
